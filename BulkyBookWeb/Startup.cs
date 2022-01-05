2d26a7c7ee0cecbc465f96e3a9579109a3918af4
girijeshts <girijeshsgowda98@gmail,com>
2022-01-05 22:22:25 +0530

    CreateDbContext all steps

--- BulkyBookWeb/Startup.cs
+++ BulkyBookWeb/Startup.cs
@@ -1,6 +1,8 @@
+using BulkyBookWeb.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,7 +26,9 @@ namespace BulkyBookWeb
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-        
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+                Configuration.GetConnectionString("Defaultconnection")
+                ));
             services.AddControllersWithViews()
                 .AddRazorRuntimeCompilation();
         }
