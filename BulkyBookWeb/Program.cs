00577e385da5d85f36329fcf9904f1a1a1c6befa
girijeshts <girijeshsgowda98@gmail,com>
2022-01-12 23:03:29 +0530

    Add dropdown list

--- BulkyBookWeb/Program.cs
+++ BulkyBookWeb/Program.cs
@@ -16,6 +16,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlSer
     builder.Configuration.GetConnectionString("DefaultConnection")
     ));
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
