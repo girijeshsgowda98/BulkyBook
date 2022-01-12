414ec7966f0d0e8eaad2498b5bbea82cde9f70d0
girijeshts <girijeshsgowda98@gmail,com>
2022-01-12 16:11:51 +0530

    IRepository interface

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -51,14 +51,14 @@ namespace BulkyBookWeb.Controllers
             {
                 return NotFound();
             }
-            var categoryFromDb = _db.Categories.Find(id);
-            //var categoryFromDbFirst = _db.Categories.FirstOrDefault(u => u.Id == id);
+            /*var categoryFromDb = _db.Categories.Find(id);*/
+            var categoryFromDbFirst = _db.Categories.FirstOrDefault(u => u.Name == "id");
             //var categoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);
-            if(categoryFromDb == null)
+            if(categoryFromDbFirst == null)
             {
                 return NotFound();
             }
-            return View();
+            return View(categoryFromDbFirst);
         }
 
         //Post
