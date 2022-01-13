5675f5b9a6149f420a4a83c59a1e913bc33c0658
girijeshts <girijeshsgowda98@gmail,com>
2022-01-13 23:32:46 +0530

    ViewData add .netCore

--- BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
@@ -63,6 +63,7 @@ namespace BulkyBookWeb.Controllers
             {
                 //create product
                 ViewBag.CategoryList = CategoryList;
+                ViewData["CoverTypeList"] = CoverTypeList;
                 return View(product);
             }
             else
